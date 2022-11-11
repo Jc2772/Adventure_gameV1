23f3fa4ad8acb83ce38105763ef73a5d488bc850
T00225039 <james.clifford2@students.ittralee>
2022-11-11 10:27:53 +0000

    gui

---
+++ project/Gui.java
@@ -0,0 +1,13 @@
+package project;
+import java.util.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public class Gui {
+    public Gui(){
+        JFrame frame = new JFrame("Adeventure game");
+        FlowLayout flow = new FlowLayout(FlowLayout.CENTER);
+        frame.setLayout(flow);
+        frame.setSize();
+    }
+}
